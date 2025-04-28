76507fbb8d1c8448caa4d235c7a17638d880ac57
Brian Blackmore <brian@mediaalpha.com>
2025-04-28 13:07:24 -0700

    CodeLayout::RequireParensWithBuiltins allow/require/permit, updated function list

--- lib/Perl/Critic/Policy/CodeLayout/RequireParensWithBuiltins.pm
+++ lib/Perl/Critic/Policy/CodeLayout/RequireParensWithBuiltins.pm
@@ -8,83 +8,74 @@ use Readonly;
 use Perl::Critic::Utils qw/ :severities :data_conversion :classification :language /;
 use base 'Perl::Critic::Policy';
 
-our $VERSION = '0.1.1';
+our $VERSION = '0.1.2';
 
 Readonly::Scalar my $DESC  => q{Builtin function called without parentheses};
 Readonly::Scalar my $EXPL  => [ 13 ];
 
-# One interpretation suggests that parentheses aren't needed when the function is followed
-# by an operator of lower precedence.  Since thi is less consistent, it has been removed
-# from the current implementation.
-# Readonly::Scalar my $lcprec => precedence_of(q{-e});
+# In each section, the order follows that of perlfunc(1).
+# Some keywords may be repeated if they appear in multiple perlfunc sections.
 
-Readonly::Array my @NAMED_UNARY_OPS => qw(
-	chdir
-	chroot
-	cos
-	glob
-	gmtime
-	hex
-	int
-	lc
-	lcfirst
-	length
-	localtime
-	log
-	lstat
-	oct
-	ord
-	quotemeta
-	rand
-	readlink
-	rmdir
-	sin
-	sleep
-	sqrt
-	srand
-	stat
-	uc
-	ucfirst
-	umask
-);
-Readonly::Hash my %NAMED_UNARY_OPS => hashify( @NAMED_UNARY_OPS );
-Readonly::Array my @ALLOW => qw(
-	alarm
+Readonly::Array my @REQUIRED => qw/
+	chr crypt fc hex index lc lcfirst length oct ord pack rindex sprintf substr uc ucfirst
+	quotemeta split study
+	abs atan2 cos exp hex int log oct rand sin sqrt srand
+	splice
+	join unpack
+	delete exists
+	binmode close closedir fileno flock read readdir readline rewinddir seek seekdir select syscall sysread sysseek syswrite tell telldir truncate warn write
+	pack read syscall sysread sysseek syswrite unpack vec
+	chdir chmod chown chroot fcntl glob ioctl link lstat mkdir open opendir readlink rename rmdir select stat symlink sysopen umask unlink utime
 	caller
-	defined
-	delete
-	do
-	eval
-	exists
+	formline lock scalar undef
+	alarm exec getpriority kill pipe readpipe setpgrp setpriority sleep system waitpid
+	bless ref tie tied untie
+	accept bind connect getpeername getsockname getsockopt listen recv send setsockopt shutdown socket socketpair
+/;
+
+Readonly::Array my @PERMITTED => qw/
+	chomp chop
+	pos
+	eof getc
 	exit
-	getgrp
-	gethostbyname
-	getnetbyname
-	getprotobyname
-	lock
-	my
-	ref
-	require
-	return
-	scalar
-	undef
-);
-Readonly::Hash my %ALLOW => hashify( @ALLOW );
+	defined reset
+	getpgrp
+	gmtime localtime time
+/;
+
+Readonly::Array my @ALLOWED => qw/
+	reverse
+	each keys pop push shift unshift values
+	each keys values
+	grep map reverse sort
+	die format print printf say
+	break continue die do dump eval evalbytes goto last next redo return wantarray
+	import local my our package state use
+	fork getppid times wait
+	do import no package require use
+	package use
+/;
 
 ####-----------------------------------------------------------------------------
 
 sub supported_parameters {
 	return (
 		{
-			name           => 'operators',
-			description    => 'The unary operators that should be restricted.',
-			default_string => join(' ',grep {!exists($ALLOW{$_})} @NAMED_UNARY_OPS),
+			name           => 'require',
+			description    => 'Always require parentheses of these operators.',
+			default_string => join(' ',@REQUIRED),
+			behavior       => 'string list',
+		},
+		{
+			name           => 'permit',
+			description    => 'Require parentheses when called with a parameter.',
+			default_string => join(' ',@PERMITTED),
 			behavior       => 'string list',
 		},
 		{
 			name           => 'allow',
-			description    => 'The unary operators that should be ignored.',
-			default_string => join(' ', @ALLOW),
+			description    => 'Never require parentheses.',
+			default_string => join(' ', @ALLOWED),
 			behavior       => 'string list',
 		},
 	);
@@ -99,11 +90,26 @@ sub applies_to           { return 'PPI::Token::Word'  }
 sub violates {
 	my ($self,$elem,undef)=@_;
 	if(!is_function_call($elem))                        { return }
-	if(!exists($$self{_operators}{ $elem->content() })) { return }
 	if(exists($$self{_allow}{ $elem->content() }))      { return }
 
 	my $sibling=$elem->snext_sibling();
-	if(!$sibling||!$sibling->isa('PPI::Structure::List')) { return $self->violation(sprintf("$DESC (%s)",$elem->content()),$EXPL,$elem) }
+	if(exists($$self{_require}{ $elem->content() })) {
+		if(!$sibling||!$sibling->isa('PPI::Structure::List')) { return $self->violation(sprintf("$DESC (%s)",$elem->content()),$EXPL,$elem) }
+	}
+
+	if(!$sibling||$sibling->isa('PPI::Structure::List')) { return }
+
+	if(exists($$self{_permit}{ $elem->content() })) {
+		if(  $sibling->isa('PPI::Token::Number')
+			|| $sibling->isa('PPI::Token::Quote')
+			|| $sibling->isa('PPI::Token::QuoteLike')
+			|| $sibling->isa('PPI::Token::Symbol')
+		) {
+			return $self->violation(sprintf("$DESC (%s)",$elem->content()),$EXPL,$elem);
+		}
+		return;
+	}
+
 	return;
 }
 
@@ -112,7 +118,6 @@ sub violates {
 1;
 
 __END__
-
 
 =pod
 
@@ -135,15 +140,29 @@ will lookup the value for C<"default"> when C<$name> is undefined, but will stil
 
 =head1 CONFIGURATION
 
-The named unary operators checked by the policy can be listed explicitly.
+The priority for configuration is Allow, Required, Permitted.
 
-	[CodeLayout::RequireParensWithBuiltins]
-	operators = lc lcfirst uc ucfirst
+=head2 Allow
 
-The default list includes most string and mathematical operators, but excludes certain system calls and block operators.  Specific named operators can be excluded:
+Functions in the C<allow> section can be called with or without parentheses, no restriction.  This overrides all other configurations.
 
 	[CodeLayout::RequireParensWithBuiltins]
 	allow = sqrt
+
+=head2 Required
+
+Names configured in the C<require> section always require parentheses, even when called without arguments or inside blocks.  EG C<lc()> or C<grep {lc($_)}>.  This overrides the C<permit> option.
+
+	[CodeLayout::RequireParensWithBuiltins]
+	operators = lc lcfirst uc ucfirst
+	require = lc lcfirst uc ucfirst
+
+=head2 Required with arguments
+
+Some functions operate on C<$_> or other defaults and may be used without parameters.  If configured in the C<permit> section, the functions will require parentheses when called with a parameter.  EG both C<grep {defined} ...> and C<if(defined($x))> are valid, but C<if(defined $x)> is a violation.
+
+	[CodeLayout::RequireParensWithBuiltins]
+	permit = defined
 
 =head1 NOTES
 
